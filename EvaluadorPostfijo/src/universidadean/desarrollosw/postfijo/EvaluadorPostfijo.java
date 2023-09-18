635be462a720ed1904b7f53f0fcfe9882f347bb8
Sandra Nova Morales <53981774+snovamo86861@users.noreply.github.com>
2023-09-18 00:14:18 -0500

    update

--- EvaluadorPostfijo/src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
+++ EvaluadorPostfijo/src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
@@ -28,31 +28,41 @@ public class EvaluadorPostfijo {
      */
     static boolean estaBalanceada(List<String> expresion) {
         Stack<String> delimitadores = new Stack<>();
+
         for (String elemento : expresion) {
-            if (esOperando(elemento)) {
-                // Si es un operando (número), ignóralo y continúa con el siguiente elemento.
-                continue;
-            } else if (esOperador(elemento)) {
+            if (esDelimitadorApertura(elemento)) {
+                delimitadores.push(elemento);
+            } else if (esDelimitadorCierre(elemento)) {
                 if (delimitadores.isEmpty()) {
-                    // Si la pila está vacía y encontramos un operador, la expresión no está balanceada.
                     return false;
                 }
 
-                // Retira un elemento de la pila y verifica si coincide con el operador actual.
-                String delimitador = delimitadores.pop();
-                if (!coincideDelimitadorOperador(delimitador, elemento)) {
+                String delimitadorTope = delimitadores.pop();
+                if (!sonPareja(delimitadorTope, elemento)) {
                     return false;
                 }
-            } else {
-                // Si el elemento no es ni operando ni operador, la expresión no está balanceada.
-                return false;
             }
         }
 
-        // Si la pila está vacía al final, la expresión está balanceada.
         return delimitadores.isEmpty();
     }
 
+    // Función para verificar si un elemento es un delimitador de apertura
+    static boolean esDelimitadorApertura(String elemento) {
+        return elemento.equals("(") || elemento.equals("[") || elemento.equals("{");
+    }
+
+    // Función para verificar si un elemento es un delimitador de cierre
+    static boolean esDelimitadorCierre(String elemento) {
+        return elemento.equals(")") || elemento.equals("]") || elemento.equals("}");
+    }
+
+    // Función para verificar si dos delimitadores son pareja (por ejemplo, '(' y ')')
+    static boolean sonPareja(String delimitadorApertura, String delimitadorCierre) {
+        return (delimitadorApertura.equals("(") && delimitadorCierre.equals(")")) ||
+                (delimitadorApertura.equals("[") && delimitadorCierre.equals("]")) ||
+                (delimitadorApertura.equals("{") && delimitadorCierre.equals("}"));
+    }
 
 
     /**
@@ -60,88 +70,121 @@ public class EvaluadorPostfijo {
      * de corchetes ([]) y llaves ({}) por paréntesis ()
      */
 
-
     static void reemplazarDelimitadores(List<String> expresion) {
         for (int i = 0; i < expresion.size(); i++) {
-            String token = expresion.get(i);
-            StringBuilder nuevoToken = new StringBuilder();
+            String elemento = expresion.get(i);
 
-            for (int j = 0; j < token.length(); j++) {
-                char caracter = token.charAt(j);
-
-                if (caracter == '[' || caracter == ']') {
-                    nuevoToken.append('('); // Reemplaza corchetes por paréntesis
-                } else if (caracter == '{' || caracter == '}') {
-                    nuevoToken.append('('); // Reemplaza llaves por paréntesis
-                } else {
-                    nuevoToken.append(caracter);
-                }
+            if (elemento.equals("[")) {
+                expresion.set(i, "(");
+            } else if (elemento.equals("]")) {
+                expresion.set(i, ")");
+            } else if (elemento.equals("{")) {
+                expresion.set(i, "(");
+            } else if (elemento.equals("}")) {
+                expresion.set(i, ")");
             }
-
-            expresion.set(i, nuevoToken.toString());
         }
     }
+
+
 
     /**
      * Realiza la conversión de la notación infija a postfija
      * @return la expresión convertida a postfija
      * OJO: Debe usarse el algoritmo que está en el enunciado OBLIGATORIAMENTE
-     */
+
     static List<String> convertirAPostfijo(List<String> expresion) {
-        Stack<String> pila = new Stack<>();
-        List<String> salida = new ArrayList<>();
+    Stack<String> pila = new Stack<>();
+    List<String> salida = new ArrayList<>();
 
-        // TODO: Escriba el algoritmo aquí
+    // TODO: Escriba el algoritmo aquí
 
-        return salida;
+    return salida;
     }
+     */
+
 
     static List<String> convertirAPostfijo(List<String> expresion) {
         Stack<String> pila = new Stack<>();
         List<String> salida = new ArrayList<>();
 
-        // Definimos la precedencia de los operadores
-        Map<String, Integer> precedencia = new HashMap<>();
-        precedencia.put("+", 1);
-        precedencia.put("-", 1);
-        precedencia.put("*", 2);
-        precedencia.put("/", 2);
-
-        for (String token : expresion) {
-            if (esOperando(token)) {
-                // Si es un operando, lo agregamos a la salida directamente
-                salida.add(token);
-            } else if (esOperador(token)) {
-                // Si es un operador, manejamos la precedencia
-                while (!pila.isEmpty() && esOperador(pila.peek()) &&
-                        precedencia.get(pila.peek()) >= precedencia.get(token)) {
+        // Recorremos la expresión de principio a fin
+        for (String termino : expresion) {
+            // Si el término es un operador, lo introducimos en la pila
+            if (esOperador(termino)) {
+                while (!pila.isEmpty() && esOperador(pila.peek()) && precedencia(termino) <= precedencia(pila.peek())) {
                     salida.add(pila.pop());
                 }
-                pila.push(token);
-            } else if (token.equals("(")) {
-                // Si es un paréntesis abierto, lo colocamos en la pila
-                pila.push(token);
-            } else if (token.equals(")")) {
-                // Si es un paréntesis cerrado, desapilamos operadores hasta encontrar el paréntesis abierto correspondiente
+                pila.push(termino);
+            }
+            // Si el término es el paréntesis que cierra ")", sacamos elementos de la pila y los agregamos a la salida
+            else if (termino.equals(")")) {
                 while (!pila.isEmpty() && !pila.peek().equals("(")) {
                     salida.add(pila.pop());
                 }
-                pila.pop(); // Quitamos el paréntesis abierto de la pila
+                // Sacamos el paréntesis que abre "(" de la pila
+                if (!pila.isEmpty() && pila.peek().equals("(")) {
+                    pila.pop();
+                }
+            }
+            // Si el término es el paréntesis que abre "(", lo introducimos en la pila
+            else if (termino.equals("(")) {
+                pila.push(termino);
+            }
+            // Cualquier otro término se agrega a la lista de salida
+            else {
+                salida.add(termino);
             }
         }
 
-        // Desapilamos cualquier operador que quede en la pila
+        // Vaciamos la pila y agregamos los operadores restantes a la salida
         while (!pila.isEmpty()) {
+            if (pila.peek().equals("(") || pila.peek().equals(")")) {
+                // Si hay paréntesis sin cerrar o abrir, hay un error en la expresión
+                return null;
+            }
             salida.add(pila.pop());
         }
 
         return salida;
     }
 
+    // Función para verificar si un término es un operador
+    static boolean esOperador(String termino) {
+        return termino.equals("+") || termino.equals("-") || termino.equals("*") || termino.equals("/") || termino.equals("%");
+    }
+
+    // Función para obtener la precedencia de un operador
+    static int precedencia(String operador) {
+        switch (operador) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+            case "%":
+                return 2;
+            default:
+                return 0; // Si no es un operador válido
+        }
+    }
+
+
+
+
+
     /**
      * Realiza la evaluación de la expresión postfijo utilizando una pila
      * @param expresion una lista de elementos con números u operadores
      * @return el resultado de la evaluación de la expresión.
+
+    static int evaluarPostFija(List<String> expresion) {
+    Stack<Integer> pila = new Stack<>();
+
+    // TODO: Realiza la evaluación de la expresión en formato postfijo
+
+    return pila.pop();
+    }
      */
 
     static int evaluarPostFija(List<String> expresion) {
@@ -149,23 +192,50 @@ public class EvaluadorPostfijo {
 
         for (String elemento : expresion) {
             if (esNumero(elemento)) {
-                // Si el elemento es un número, lo conviertes a Integer y lo apilas en la pila
+                // Si es un número, lo introducimos en la pila
                 pila.push(Integer.parseInt(elemento));
             } else {
-                // Si el elemento es un operador, desapilas los dos últimos números de la pila
+                // Es un operador
                 int operand2 = pila.pop();
                 int operand1 = pila.pop();
-
-                // Realizas la operación correspondiente y apilas el resultado en la pila
-                int resultado = realizarOperacion(operand1, operand2, elemento);
+                int resultado = aplicarOperador(elemento, operand1, operand2);
                 pila.push(resultado);
             }
         }
 
-        // Al final, la pila debe contener el resultado final de la expresión
+        // Al final, el resultado estará en el tope de la pila
         return pila.pop();
     }
 
+    static boolean esNumero(String str) {
+        // Verifica si la cadena es un número
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    static int aplicarOperador(String operador, int operand1, int operand2) {
+        // Realiza la operación según el operador
+        switch (operador) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) {
+                    throw new ArithmeticException("División por cero");
+                }
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Operador no válido: " + operador);
+        }
+    }
+
+
 
 }
-
